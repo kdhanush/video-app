28430b812fd8d69303bfd40aac28b75c35e99fce
Dhanush K <dhanushk1998@gmail.com>
2024-02-14 22:48:53 +0530

    Initial setup

---
+++ src/components/Videoplayer/VideoPlayer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function VideoPlayer() {
+  return (
+    <>
+     <div className='video-container'>
+      <h1>Video Player</h1>
+      <video src=""></video>
+      </div>
+    </>
+  )
+}
+
+export default VideoPlayer
---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import VideoPlayer from '../../components/Videoplayer/VideoPlayer'
+
+function Home() {
+  return (
+   <>
+   <VideoPlayer />
+   </>
+  )
+}
+
+export default Home
